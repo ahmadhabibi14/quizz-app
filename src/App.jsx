13455362781919be215c851fa5c2ad87f625f2e2
ahmadhabibi14 <ahmadhabibi7159@gmail.com>
2023-04-22 23:06:01 +0800

    Test timer 01

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 // import axios from "axios";
 import "./index.css";
 import { TriviaData } from "./apis/quiz";
@@ -15,6 +15,46 @@ function App() {
       wrongAnswer: 0,
       totalAnswer: 0
    })
+   const [timer, setTimer] = useState("00:00");
+   const Ref = useRef(null);
+
+   const getTimeRemaining = (e) => {
+      const total = Date.parse(e) - Date.parse(new Date());
+      const seconds = Math.floor((total / 1000) % 60);
+      const minutes = Math.floor((total / 1000 / 60) % 60);
+      return {
+         total, minutes, seconds
+      }
+   }
+
+   const startTimer = (e) => {
+      let { total, minutes, seconds } = getTimeRemaining(e);
+      if (total >= 0) {
+         setTimer(
+            (minutes > 9 ? minutes : '0'+minutes) + ':' +
+            (seconds > 9 ? seconds : '0'+seconds)
+         )
+      }
+   }
+
+   const clearTimer = (e) => {
+      setTimer("15:00");
+      if (Ref.current) clearInterval(Ref.current);
+      const id = setInterval(() => {
+         startTimer(e)
+      }, 1000);
+      Ref.current = id;
+   }
+
+   const getDateTime = () => {
+      let deadline = new Date();
+      deadline.setSeconds(deadline.getSeconds() + 10);
+      return deadline;
+   }
+
+   const clickOnReset = () => {
+      clearTimer(getDateTime());
+   }
 
    async function combineAllAnswers() {
       let allAnswers = [];
@@ -31,6 +71,8 @@ function App() {
       combineAllAnswers();
       console.log(triviaData[currentQuestion].incorrect_answers)
       console.log(currentQuestion)
+
+      clearTimer(getDateTime());
    }, []);
 
    const NextQuestion = () => {
@@ -73,27 +115,24 @@ function App() {
       console.log(answerCorrect)
    }
    
-   // console.log(allPossibleAnswers[selectedAnswerIndex])
    return (
       <div className="bg-zinc-950 min-h-screen text-slate-50 flex justify-center py-16">
          <div className="flex flex-col space-y-6 w-5/12">
-            <h1 className="text-sm py-4 px-10 flex flex-row justify-between rounded-xl bg-zinc-900 font-bold text-center">
-               <span>Quiz App</span>
-               <div className="flex flex-col space-y-2">
+            <h1 className="py-4 px-10 flex flex-row justify-between items-center rounded-xl bg-zinc-900 text-center">
+               <span className="text-3xl text-blue-500 font-black">Quiz App</span>
+               <div className="flex flex-col space-y-2 text-sm">
                   <p>Total Answer: {result.totalAnswer}</p>
                   <p>Wrong answer: {result.wrongAnswer}</p>
                   <p>Correct answer: {result.correctAnswer}</p>
                </div>
             </h1>
+            <p>{timer}</p>
+            <button className="p-2 bg-zinc-800" onClick={clickOnReset}>Reset</button>
             <div className="flex flex-col space-y-3 bg-zinc-900 rounded-xl p-4">
                
                <div className="flex flex-col space-y-3">
                   <h2 className="text-lg font-bold">{removeCharacters(triviaData[currentQuestion].question)}</h2>
                   <ul className="flex flex-col space-y-3 list-none">
-                     {/* {triviaData[currentQuestion].incorrect_answers.map((answer, index) => (
-                        <li onClick={clickAnswer} className={activeAnswer ? 'answer-active' : 'answer'} key={index}>{answer}</li>
-                     ))}
-                     <li onClick={clickAnswer} className={activeAnswer ? 'answer-active' : 'answer'}>{triviaData[currentQuestion].correct_answer}</li> */}
                      {allPossibleAnswers.map((answer, index) => (
                         <li onClick={()=> clickAnswer(answer, index)}
                            className={selectedAnswerIndex === index ? 'answer-active' : 'answer'}
@@ -105,7 +144,7 @@ function App() {
                   </ul>
                </div>
                <div className="flex flex-row justify-end pr-4">
-                  <button onClick={NextQuestion} className="py-2 px-6 bg-blue-600 rounded-xl w-fit">
+                  <button onClick={NextQuestion} className="py-2 px-6 bg-blue-600 hover:bg-blue-700 rounded-xl w-fit">
                      {currentQuestion === triviaData.length - 1 ? 'Finish' : 'Next'}
                   </button>
                </div>
