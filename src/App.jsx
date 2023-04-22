9c537c3f6a270c1fa85087b0583134a22645f022
ahmadhabibi14 <ahmadhabibi7159@gmail.com>
2023-04-22 16:33:09 +0800

    Fix answer logic

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ import { TriviaData } from "./apis/quiz";
 
 function App() {
    const triviaData = TriviaData
-   const [currentQuestion, setCurrentQuestion] = useState(0);
+   let [currentQuestion, setCurrentQuestion] = useState(0);
    const [allPossibleAnswers, setAllPossibleAnswers] = useState([]);
    const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
-   const [selectedAnswer, setSelectedAnswer] = useState(true);
+   const [selectedAnswer, setSelectedAnswer] = useState(false);
+   const [answerCorrect, setAnswerCorrect] = useState(false);
    const [result, setResult] = useState({
       correctAnswer: 0,
       wrongAnswer: 0,
@@ -28,45 +29,61 @@ function App() {
 
    useEffect(() => {
       combineAllAnswers();
+      console.log(triviaData[currentQuestion].incorrect_answers)
+      console.log(currentQuestion)
    }, []);
 
    const NextQuestion = () => {
       if (currentQuestion < triviaData.length) {
-         setCurrentQuestion(currentQuestion + 1);
+         setCurrentQuestion(currentQuestion += 1);
          combineAllAnswers();
-         if (allPossibleAnswers[selectedAnswerIndex] === triviaData[currentQuestion].correct_answer) {
-            setResult({
+         setResult(( {correctAnswer, wrongAnswer, totalAnswer} ) => (
+            answerCorrect
+            ? {
                correctAnswer: correctAnswer + 1,
-               totalAnswer: totalAnswer + 1
-            })
-         } else {
-            setResult({
+               wrongAnswer: wrongAnswer,
                totalAnswer: totalAnswer + 1,
-               wrongAnswer: wrongAnswer + 1
-            })
-         }
+            } : {
+               correctAnswer: correctAnswer,
+               wrongAnswer: wrongAnswer + 1,
+               totalAnswer: totalAnswer + 1,
+            }
+         ))
          setSelectedAnswerIndex(null)
       } else {
-         setCurrentQuestion(0)
+         setCurrentQuestion(0);
+         setSelectedAnswerIndex(null)
       }
+      console.log(currentQuestion)
    }
 
    function removeCharacters(question) {
       return question.replace(/(&quot\;)/g, "\"").replace(/(&rsquo\;)/g, "\"").replace(/(&#039\;)/g, "\'").replace(/(&amp\;)/g, "\"");
    }
 
-   function clickAnswer(index) {
+   function clickAnswer(answer, index) {
       setSelectedAnswerIndex(index)
-      setSelectedAnswer(!selectedAnswer)
+      if (answer === triviaData[currentQuestion].correct_answer) {
+         setAnswerCorrect(true)
+      } else {
+         setAnswerCorrect(false)
+      }
+      setSelectedAnswer(true)
+      console.log(answer)
+      console.log(answerCorrect)
    }
    
-   // console.log(triviaData[0])
+   // console.log(allPossibleAnswers[selectedAnswerIndex])
    return (
       <div className="bg-zinc-950 min-h-screen text-slate-50 flex justify-center py-16">
          <div className="flex flex-col space-y-6 w-5/12">
-            <h1 className="text-xl py-4 px-10 flex flex-row justify-between rounded-xl bg-zinc-900 font-bold text-center">
+            <h1 className="text-sm py-4 px-10 flex flex-row justify-between rounded-xl bg-zinc-900 font-bold text-center">
                <span>Quiz App</span>
-               <span>Score : {result.totalAnswer}</span>
+               <div className="flex flex-col space-y-2">
+                  <p>Total Answer: {result.totalAnswer}</p>
+                  <p>Wrong answer: {result.wrongAnswer}</p>
+                  <p>Correct answer: {result.correctAnswer}</p>
+               </div>
             </h1>
             <div className="flex flex-col space-y-3 bg-zinc-900 rounded-xl p-4">
                
@@ -78,7 +95,7 @@ function App() {
                      ))}
                      <li onClick={clickAnswer} className={activeAnswer ? 'answer-active' : 'answer'}>{triviaData[currentQuestion].correct_answer}</li> */}
                      {allPossibleAnswers.map((answer, index) => (
-                        <li onClick={()=> clickAnswer(index)}
+                        <li onClick={()=> clickAnswer(answer, index)}
                            className={selectedAnswerIndex === index ? 'answer-active' : 'answer'}
                            key={index}
                         >
