683cb3f314fb847c3e0a16cecd5afbd175ce4e13
ahmadhabibi14 <ahmadhabibi7159@gmail.com>
2023-04-24 20:36:59 +0800

    Question and Answer done

--- src/App.jsx
+++ src/App.jsx
@@ -4,55 +4,23 @@ import "./index.css";
 import { TriviaData } from "./apis/quiz";
 function App() {
    const triviaData = TriviaData
-   // React HOOK
+   // useState HOOK
    const [showResult, setShowResult] = useState(false);
-   let [currentQuestion, setCurrentQuestion] = useState(0);
    const [allPossibleAnswers, setAllPossibleAnswers] = useState([]);
-   const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
-   const [selectedAnswer, setSelectedAnswer] = useState(false);
+   let [currentQuestion, setCurrentQuestion] = useState(0);
    const [answerCorrect, setAnswerCorrect] = useState(false);
    const [result, setResult] = useState({
       correctAnswer: 0,
       wrongAnswer: 0,
       totalAnswer: 0
    })
-   const [timer, setTimer] = useState("00:00");
+   // useEffect HOOK
    useEffect(() => {
       combineAllAnswers();
-      console.log(triviaData[currentQuestion].incorrect_answers)
-      console.log(currentQuestion)
-
-      clearTimer(getDateTime());
+      console.log(triviaData.length)
    }, []);
-   const Ref = useRef(null);
 
-   const getTimeRemaining = (e) => {
-      const total = Date.parse(e) - Date.parse(new Date());
-      const seconds = Math.floor((total / 1000) % 60);
-      const minutes = Math.floor((total / 1000 / 60) % 60);
-      return { total, minutes, seconds }
-   }
-   const startTimer = (e) => {
-      let { total, minutes, seconds } = getTimeRemaining(e);
-      if (total >= 0) {
-         setTimer((minutes > 9 ? minutes : '0'+minutes) + ':' + (seconds > 9 ? seconds : '0'+seconds))
-      }
-   }
-   const clearTimer = (e) => {
-      setTimer("15:00");
-      if (Ref.current) clearInterval(Ref.current);
-      const id = setInterval(() => { startTimer(e) }, 1000);
-      Ref.current = id;
-   }
-   const getDateTime = () => {
-      let deadline = new Date();
-      deadline.setSeconds(deadline.getSeconds() + 900);
-      return deadline;
-   }
-   const clickOnReset = () => { clearTimer(getDateTime());}
-
-
-   async function combineAllAnswers() {
+   function combineAllAnswers() {
       let allAnswers = [];
       let correctAnswer = triviaData[currentQuestion].correct_answer
       triviaData[currentQuestion].incorrect_answers.map((answer) => { allAnswers.push(answer) });
@@ -60,11 +28,14 @@ function App() {
       allAnswers.sort(() => Math.random() - 0.5);
       setAllPossibleAnswers(allAnswers);
    }
-
-   const NextQuestion = () => {
-      if (currentQuestion !== triviaData.length) {
-         setCurrentQuestion(currentQuestion += 1);
-         combineAllAnswers();
+   function removeCharacters(question) {
+      return question.replace(/(&quot\;)/g, "\"").replace(/(&rsquo\;)/g, "\"").replace(/(&#039\;)/g, "\'").replace(/(&amp\;)/g, "\"");
+   }
+   function clickAnswer(answer) {
+      if (currentQuestion !== triviaData.length - 1) {
+         answer === triviaData[currentQuestion].correct_answer
+            ?  setAnswerCorrect(true)
+            :  setAnswerCorrect(false)
          setResult(( {correctAnswer, wrongAnswer, totalAnswer} ) => (
             answerCorrect ? {
                correctAnswer: correctAnswer + 1,
@@ -76,25 +47,22 @@ function App() {
                totalAnswer: totalAnswer + 1,
             }
          ))
-         setSelectedAnswerIndex(null)
+         setCurrentQuestion(currentQuestion += 1);
+         combineAllAnswers();
       } else {
+         setResult(( {correctAnswer, wrongAnswer, totalAnswer} ) => (
+            answerCorrect ? {
+               correctAnswer: correctAnswer + 1,
+               wrongAnswer: wrongAnswer,
+               totalAnswer: totalAnswer + 1,
+            } : {
+               correctAnswer: correctAnswer,
+               wrongAnswer: wrongAnswer + 1,
+               totalAnswer: totalAnswer + 1,
+            }
+         ))
          setShowResult(true);
       }
-      console.log(currentQuestion)
-   }
-   function removeCharacters(question) {
-      return question.replace(/(&quot\;)/g, "\"").replace(/(&rsquo\;)/g, "\"").replace(/(&#039\;)/g, "\'").replace(/(&amp\;)/g, "\"");
-   }
-   function clickAnswer(answer, index) {
-      setSelectedAnswerIndex(index)
-      if (answer === triviaData[currentQuestion].correct_answer) {
-         setAnswerCorrect(true)
-      } else {
-         setAnswerCorrect(false)
-      }
-      setSelectedAnswer(true)
-      console.log(answer)
-      console.log(answerCorrect)
    }
 
    return (
@@ -105,15 +73,15 @@ function App() {
                <h1 className="py-4 rounded-xl bg-zinc-900 text-blue-600 text-3xl font-black cursor-pointer text-center">Quiz App</h1>            
                <div className="flex flex-col space-y-3 bg-zinc-900 rounded-xl p-4">
                   <div className="flex flex-row justify-between border-b border-zinc-800 py-4 text-xl font-bold">
-                     <div>Question {(currentQuestion > 9 ? currentQuestion + 1 : '0' + (currentQuestion + 1))}<span className="text-blue-600">/</span><span className="text-zinc-400 text-base">{triviaData.length + 1}</span></div>
-                     <div>{timer}</div>
+                     <div>Question {(currentQuestion >= 9 ? (currentQuestion + 1) : '0'+(currentQuestion + 1))}<span className="text-blue-600">/</span><span className="text-zinc-400 text-base">{triviaData.length}</span></div>
+                     <div>15:00</div>
                   </div>
                   <div className="flex flex-col space-y-6">
-                     <h2 className="text-lg font-bold">{removeCharacters(triviaData[currentQuestion]?.question)}</h2>
+                     <h2 className="text-lg">{removeCharacters(triviaData[currentQuestion]?.question)}</h2>
                      <ul className="flex flex-col space-y-3 list-none">
                         {allPossibleAnswers.map((answer, index) => (
-                           <li onClick={()=> clickAnswer(answer, index)}
-                              className={selectedAnswerIndex === index ? 'answer-active' : 'answer'}
+                           <li onClick={()=> clickAnswer(answer)}
+                              className="answer"
                               key={index}
                               >
                               {answer}
@@ -121,18 +89,13 @@ function App() {
                         ))}
                      </ul>
                   </div>
-                  <div className="flex flex-row justify-end pr-4">
-                     <button onClick={NextQuestion} className="py-2 px-6 bg-blue-600 hover:bg-blue-700 rounded-xl w-fit">
-                        {currentQuestion === triviaData.length + 1 ? 'Finish' : 'Next'}
-                     </button>
-                  </div>
                </div>
-               <button className="p-2 bg-zinc-800" onClick={clickOnReset}>Reset</button>
             </div>
          ) : (
-            <div>
-               {/* {result.correctAnswer} */}
-               tess
+            <div className="flex flex-col space-y-4 w-5/12">
+               <p className="text=lg font-bold">Total Answer {result.totalAnswer}</p>
+               <p className="text=lg font-bold">Correct Answer {result.correctAnswer}</p>
+               <p className="text=lg font-bold">Wrong Answer {result.wrongAnswer}</p>
             </div>
          )}
       </div>
